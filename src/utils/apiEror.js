d7988436907f028d55524eed8a457a73ba666076
bhupendra gariya <bhupiigarya7@gmail.com>
2025-04-09 15:49:49 +0530

    utils

---
+++ src/utils/apiEror.js
@@ -0,0 +1,25 @@
+class apiError extends Error {
+    constructor( 
+        message,
+         statusCode, 
+         errors = [],
+    stack = "") {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.data = null;
+        this.success = false;
+
+
+        if (stack) {
+            this.stack = stack;
+        } else{
+            Error.captureStackTrace(this, this.constructor);
+
+        }
+
+    }
+}
+
+export { apiError}
+
